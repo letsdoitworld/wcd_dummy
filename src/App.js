7e76ce9887685dc977c279fe0e250ecaae4c14ba
Andrei Abozau <andrei@multiloginapp.com>
2017-10-28 13:55:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+      sdds
+
+  }
+}
+
+export default App;
